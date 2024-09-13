30f51116b22e73273fbf56345ed21f7ad612ba16
Mittelblut9 <sauer.benedikt@outlook.de>
2024-09-13 15:38:55 +0200

    feat(KRO-4): add skeleton

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,23 @@
-<template>
-    <div v-if="streamData.type">
+<template> <!--streamData.type-->
+    <div>
         <OnlineIntendComponent
             v-if="streamData.type === 'offline'"
             :stream-data="streamData"
         />
         <IsLiveComponent
-            v-else
+            v-else-if="streamData.type === 'live'"
             :stream-data="streamData"
         />
+        <div v-else class="flex justify-center align-center mt-28">
+            <div>
+                <div class="flex justify-center">
+                    <USkeleton class="h-32 w-[200px]" />
+                </div>
+                <div class="mt-10">
+                    <USkeleton class="h-4 w-[600px]" />
+                </div>
+            </div>
+        </div>
         <div class="flex justify-center">
             <div class="md:w-1/2 w-11/12 mt-20">
                 <FaqComponent />
